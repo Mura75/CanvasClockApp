fcadd476382c39a86b84ae3afe35f7677fe948bf
murager <murager@codebusters.team>
2019-11-05 08:13:21 +0600

    Init commit.

---
+++ app/src/main/java/com/mobile/advancedcanvas/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mobile.advancedcanvas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
