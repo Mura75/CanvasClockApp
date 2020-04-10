3ea4c7c80c68685b5c350338c8711f0ac9a95f1f
murager <murager@codebusters.team>
2020-04-10 11:21:40 +0600

    Clock view optimized.

--- app/src/main/java/com/mobile/advancedcanvas/MyClockView.kt
+++ app/src/main/java/com/mobile/advancedcanvas/MyClockView.kt
@@ -6,9 +6,11 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Rect
 import android.util.AttributeSet
+import android.util.Log
 import android.util.TypedValue
 import android.view.View
 import java.util.*
+import kotlin.collections.HashMap
 
 class MyClockView : View {
 
@@ -18,11 +20,14 @@ class MyClockView : View {
         SECOND(3)
     }
 
-    private lateinit var paint: Paint
+    private val paint: Paint = Paint()
     private var radius = 0f
     private var fontSize = 0f
 
     private var numbers = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+
+    private var numbersPositionsX = FloatArray(numbers.size)
+    private var numbersPositionsY = FloatArray(numbers.size)
 
     private var rect = Rect()
 
@@ -35,52 +40,58 @@ class MyClockView : View {
 
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
 
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-        init()
-
-        canvas.drawColor(Color.WHITE)
-        drawCircle(canvas)
-        drawNumber(canvas)
-        drawArrows(canvas)
-
-        postInvalidateDelayed(500)
-        invalidate()
-    }
-
-    private fun init() {
+    init {
         fontSize = TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_SP,
             19f,
             resources.displayMetrics
         )
-        val min = Math.min(height, width).toFloat()
-        radius = min / 2 - 50
-        paint = Paint()
-        arrowTruncation = min / 20
-        hourArrowTruncation = min / 7
     }
 
-    private fun drawCircle(canvas: Canvas) {
-        paint.reset()
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+
         paint.setColor(Color.BLACK)
         paint.strokeWidth = 5f
         paint.style = Paint.Style.STROKE
+
+        val min = Math.min(height, width).toFloat()
+        radius = min / 2 - 50
+        arrowTruncation = min / 20
+        hourArrowTruncation = min / 7
+
+        paint.textSize = fontSize
+        numbers.forEachIndexed { index, number ->
+            val angle = Math.PI / 6 * (number - 3)
+            val x = (width / 2 + Math.cos(angle) * radius - rect.width() / 2).toFloat() - 15
+            val y = (height / 2 + Math.sin(angle) * radius + rect.height() / 2).toFloat() + 15
+            numbersPositionsX[index] = x
+            numbersPositionsY[index] = y
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        drawCircle(canvas)
+        drawNumber(canvas)
+        drawArrows(canvas)
+        postInvalidateDelayed(500)
+    }
+
+    private fun drawCircle(canvas: Canvas) {
         canvas.drawCircle(
             (width / 2).toFloat(),
             (height / 2).toFloat(),
-            radius + padding - 10,
+            radius + padding - 0,
             paint
         )
     }
 
     private fun drawNumber(canvas: Canvas) {
-        paint.textSize = fontSize
-        numbers.forEach { number ->
+        numbers.forEachIndexed { index, number ->
             paint.getTextBounds(number.toString(), 0, number.toString().length, rect)
-            val angle = Math.PI / 6 * (number - 3)
-            val x = (width / 2 + Math.cos(angle) * radius - rect.width() / 2).toFloat()
-            val y = (height / 2 + Math.sin(angle) * radius + rect.height() / 2).toFloat()
+            val x = numbersPositionsX[index]
+            val y = numbersPositionsY[index]
             canvas.drawText(number.toString(), x, y, paint)
         }
     }
